6f0108493cf132c7a4ec339e280ff8b6a358ee33
colm harrington <colm.harrington@gmail.com>
2024-07-25 00:37:37 +0100

    custom 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 const owner = "astrojs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind()]
 });
