89cef8c8cec403a7ddc101ebea32f0a194fe07e1
colm harrington <colm.harrington@gmail.com>
2025-09-01 21:03:54 +0100

    formatting

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,17 +2,17 @@
 
 /** @type {import("prettier").Config} */
 export default {
-        plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-        overrides: [
-                {
-                        files: '*.astro',
-                        options: {
-                                parser: 'astro',
-                        },
-                },
-        ],
-        trailingComma: 'es5',
-        tabWidth: 2,
-        semi: false,
-        singleQuote: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
 }
